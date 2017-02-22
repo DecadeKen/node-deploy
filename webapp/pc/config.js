929646962efad9e19ed5e46a95703c6b6d46c3e3
DecadeKen <177038849@qq.com>
2017-02-22 09:44:39 +0800

    webapp配置

--- webapp/pc/config.js
+++ webapp/pc/config.js
@@ -9,7 +9,11 @@ var config = {
             "remoteDir": '/root/test/',
             "backupDir": '/Users/ken/workSpace/p2p/backup'
         }
-    }]
+    }],
+    "svnUrl": [
+        'https://nfd-server02/svn/p2p/preRelease',
+        'ttps://nfd-server02/svn/activity/preRelease'
+    ]
 };
 
 module.exports = config;
