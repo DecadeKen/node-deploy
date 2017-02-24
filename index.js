743150a9dbb188ede12b0fe1acdd4a7d852b29c2
DecadeKen <177038849@qq.com>
2017-02-24 13:41:49 +0800

    上传错误状态添加

--- index.js
+++ index.js
@@ -4,16 +4,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var childProcess = require('child_process');
 require('express-ws')(app);
 
+// var serverName = "./lib/router.js",
+// mainServer = childProcess.fork(serverName)(app);
 app.use(express.static(path.resolve(__dirname, './webapp/pc-dev/')));
 
-require('./lib/service/router.js')(app, {
-    pageStatic: __dirname, //页面资源文件夹
-    resourceStatic: __dirname //静态资源文件夹
-});
 
 
+
+
+
+require('./lib/service/router.js')(app);
 
 
 app.listen(5000, function() {
