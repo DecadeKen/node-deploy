1db92085aa32db276fd09bebfebc47f44ac834f0
DecadeKen <177038849@qq.com>
2017-03-01 13:55:56 +0800

    add

--- webapp/pc/config.js
+++ webapp/pc/config.js
@@ -9,6 +9,17 @@ var config = {
             "remoteDir": '/root/test/',
             "backupDir": '/Users/node_deploy/backup'
         }
+    }, {
+        "host": '192.168.1.7',
+        "port": 22,
+        "username": 'root',
+        "password": 'nongfadai',
+        "uploadOpt": {
+            "localDir": '/Users/node_deploy/',
+            // "remoteDir": '/app/soft/pc/static/',
+            "remoteDir": '/root/test/',
+            "backupDir": '/Users/node_deploy/backup',
+        }
     }],
     "svnUrl": [
         'https://nfd-server02/svn/p2p/preRelease/',
